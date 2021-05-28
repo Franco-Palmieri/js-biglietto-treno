1e618899199e55b56779d73fe5bc04f6c8dfdefd
Ncesecrede <francescopm95@gmail.com>
2021-05-28 16:52:23 +0100

    Biglietto

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
-var age = prompt ("inserisci età")
-var km = prompt ("quanti km vuoi percorrere?")
-var x = 0,21
+var age = prompt ("inserisci età");
+var km = prompt ("quanti km vuoi percorrere?");
+var x = 0.21;
 var prezzo = parseInt(km) * parseInt(x);
+var scontoVenti = parseInt(prezzo) * 20 / 100;
+var scontoQuaranta = parseInt(prezzo) * 40 / 100;
 
-age = parseInt(age)
-km = parseInt (km)
-x = parseInt(x)
+age = parseInt(age);
+km = parseInt(km);
+x = parseInt(x);
+venti = parseInt(venti);
+quaranta = parseInt(quaranta);
+
+if (age < 18){
+    document.getElementById("prezzoTotale").innerHTML = "ciao" + prezzo + scontoVenti;
+}
