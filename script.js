325fe877c5628fb6881ea382dcb33c2a486c85d1
Ncesecrede <francescopm95@gmail.com>
2021-05-28 16:28:52 +0100

    Biglietto

---
+++ script.js
@@ -0,0 +1,8 @@
+var age = prompt ("inserisci età")
+var km = prompt ("quanti km vuoi percorrere?")
+var x = 0,21
+var prezzo = parseInt(km) * parseInt(x);
+
+age = parseInt(age)
+km = parseInt (km)
+x = parseInt(x)
